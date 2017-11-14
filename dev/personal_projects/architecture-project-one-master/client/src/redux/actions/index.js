ec0cae485f1d90ce5473375a83cd4b4c372be077
Mak Karmarkar <mkarmarkar@gmail.com>
2017-11-14 15:27:59 -0700

    Move git folder to correct place

--- dev/personal_projects/architecture-project-one-master/client/src/redux/actions/index.js
+++
@@ -1,8 +0,0 @@
-import { addWord, deleteWord, toggleWord, setWords, toggleIsShowForm } from './words';
-export const words = {
-  addWord,
-  deleteWord,
-  toggleIsShowForm,
-  toggleWord,
-  setWords
-}
--- dev/personal_projects/architecture-project-one-master/client/src/redux/reducers/isShowForm.js
+++
@@ -1,6 +0,0 @@
-export const isShowForm = (state = true, action) => {
-    if (action.type === 'TOGGLE_IS_SHOW_FORM') {
-        return !state;
-    }
-    return state;
-}
