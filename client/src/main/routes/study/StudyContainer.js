4311bccb071ecebbac782194e40479abde2afc1c
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-11-17 14:52:45 -0800

    Make other Study route on navbar, create StudyComponent and StudyContainer. Ready to catch all words user wants. At StudyComponent have Show/Hide button, will toggle that words. Repeat button click to keep word, or Got it button to passed word, untill they passed all words. Will display CONGRATULATIONS!!! and show STUDY AGAIN button if they want

---
+++ client/src/main/routes/study/StudyContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import StudyComponent from './StudyComponent';
+
+
+class StudyContainer extends Component {
+    render() {
+        return (
+            <StudyComponent />
+        )
+    }
+
+}
+
+export default StudyContainer;
