f862d7a9a0f301b3b7763e5ce3ba43a28b03fb88
Mak Karmarkar <mkarmarkar@gmail.com>
2017-11-16 17:23:17 -0700

    style elements for login signup

--- client/src/main/routes/about/AboutComponent.js
+++ client/src/main/routes/about/AboutComponent.js
@@ -1,10 +1,25 @@
 import React from "react";
+import "./about.css";
+import "./about-img.jpg";
 
 function AboutComponent(){
     return(
         <div className="about-wrapper">
-          <p> This app is a virtual flash card collection
-          for learning new language. User can add new flash card
+          <p> This app is a virtual flashcard collection
+          for learning new language. User can:
+              <ul>
+                  <li> Study preloaded  flashcards </li>
+                  <li> Add new flashcard </li>
+                  <li> Track their progress  </li>
+              </ul>
+          </p>
+          <img className="about-bg" src={"./about-img.jpg"} alt="kids vocabulary"/>
+          <p> The app is made by <i> Mak and Phat</i> using:
+              <ul>
+                  <li> User authentication by React token  </li>
+                  <li> ReactJS on front-end for user interface and interaction  </li>
+                  <li> NodeJS and Express for  Back-end with MongoDB for noSQL database manager</li>
+              </ul>
           </p>
         </div>
     )
--- client/src/main/routes/signup/SignUpComponent.js
+++ client/src/main/routes/signup/SignUpComponent.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "./signup.css";
 
 export default class SignupComponent extends Component {
     render() {
         return (
-            <div className="form-wrapper">
+            <div className="signup-form-wrapper">
                 <form onSubmit={this.props.handleSubmit}>
                     <h3>Sign Up</h3>
-                    <input onChange={this.props.handleChange} value={this.props.username} name="username" type="text" placeholder="@Username" />
-                    <input onChange={this.props.handleChange} value={this.props.email} name="email" type="email" placeholder="Email" />
-                    <input onChange={this.props.handleChange} value={this.props.password} name="password" type="password" placeholder="#" />
+                    <input onChange={this.props.handleChange} value={this.props.username} name="username" type="text" placeholder="@Username" /><br/><br/>
+                    <input onChange={this.props.handleChange} value={this.props.email} name="email" type="email" placeholder="Email" /><br/><br/>
+                    <input onChange={this.props.handleChange} value={this.props.password} name="password" type="password" placeholder="#" /><br/><br/>
                     <button type="submit">Create Account</button>
                     <p>{this.props.errMsg}</p>
                 </form>
