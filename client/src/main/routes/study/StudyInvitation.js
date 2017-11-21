72cc5ebddcbfa83f223006742bd44604eec5ce5f
Mak Karmarkar <mkarmarkar@gmail.com>
2017-11-21 16:50:46 -0700

    remove two study now buttons in learn section

--- client/src/main/routes/study/StudyInvitation.js
+++ client/src/main/routes/study/StudyInvitation.js
@@ -5,20 +5,19 @@ function StudyInvitation(props) {
     const { isFirstTime, startPlay } = props;
     if (isFirstTime)
     return (
-        <div>
-            <button className="btn btn-success" onClick={startPlay}>Study now!</button>
+
         <div className="study-wrap">
             <button className="btn btn-success" onClick={startPlay}>Study now!</button>
         </div>
-        </div>
+
     );
     return (
-        <div>
+
         <div className="study-wrap">
             <p>Congratulations!</p>
             <button className="btn btn-success" onClick={startPlay}>Study again</button>
         </div>
-        </div>
+        
     );
 }
 
